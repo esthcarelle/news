8d7e00817839ff36f4ca5c6c23d8d58a0ce4d1df
Esther Carrelle <esthcarelle@gmail.com>
2023-11-10 19:53:03 +0200

    Added the service class and the ApiUrl class

---
+++ app/src/main/java/com/mine/news/constants/ApiUrl.kt
@@ -0,0 +1,7 @@
+package com.mine.news.constants
+
+object ApiURL {
+    const val API_KEY = "3394d742efc24cf899246261e8fb89a0"
+    const val BASE_URL = "https://newsapi.org"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w342"
+}
